4935f6a27b68a563ff29472bfd603dc0725f3f13
Noah Hüsser <yatekii@yatekii.ch>
2020-03-27 22:07:46 +0100

    Enable clocks; still doesn't work

--- src/main.rs
+++ src/main.rs
@@ -8,44 +8,45 @@ pub use stm32mp1_pac as target;
 
 #[entry]
 fn main() -> ! {
-    if let Some(p) = target::Peripherals::take() {
-        let i = 8;
-        let offset = 2 * i;
-        unsafe {
-            &(*target::GPIOD::ptr())
-                .gpiox_pupdr
-                .modify(|r, w| w.bits((r.bits() & !(0b11 << offset)) | (0b01 << offset)));
-            &(*target::GPIOD::ptr())
-                .gpiox_otyper
-                .modify(|r, w| w.bits(r.bits() & !(0b1 << i)));
-            &(*target::GPIOD::ptr())
-                .gpiox_moder
-                .modify(|r, w| w.bits((r.bits() & !(0b11 << offset)) | (0b01 << offset)));
-        }
+    let i = 8;
+    let offset = 2 * i;
+    unsafe {
+        &(*target::RCC::ptr())
+            .rcc_mc_ahb3ensetr
+            .write(|w| w.hsemen().set_bit());
+        &(*target::RCC::ptr())
+            .rcc_mc_ahb4ensetr
+            .write(|w| w.gpioeen().set_bit());
 
-        unsafe { (*target::GPIOD::ptr()).gpiox_bsrr.write(|w| w.bits(1 << i)) }
+        &(*target::GPIOD::ptr())
+            .gpiox_pupdr
+            .modify(|r, w| w.bits((r.bits() & !(0b11 << offset)) | (0b01 << offset)));
+        &(*target::GPIOD::ptr())
+            .gpiox_otyper
+            .modify(|r, w| w.bits(r.bits() & !(0b1 << i)));
+        &(*target::GPIOD::ptr())
+            .gpiox_moder
+            .modify(|r, w| w.bits((r.bits() & !(0b11 << offset)) | (0b01 << offset)));
+    }
 
-        let mut count: u8 = 0;
-        loop {
-            count += 1;
+    unsafe { (*target::GPIOD::ptr()).gpiox_bsrr.write(|w| w.bits(1 << i)) }
 
-            if count & 1 == 1 {
-                unsafe { (*target::GPIOD::ptr()).gpiox_bsrr.write(|w| w.bits(1 << i)) }
-            } else {
-                unsafe {
-                    (*target::GPIOD::ptr())
-                        .gpiox_bsrr
-                        .write(|w| w.bits(1 << (i + 16)))
-                };
-            }
-
-            for _ in 0..1_000_000 {
-                cortex_m::asm::nop();
-            }
-        }
-    };
-
+    let mut count: u8 = 0;
     loop {
-        continue;
+        count += 1;
+
+        if count & 1 == 1 {
+            unsafe { (*target::GPIOD::ptr()).gpiox_bsrr.write(|w| w.bits(1 << i)) }
+        } else {
+            unsafe {
+                (*target::GPIOD::ptr())
+                    .gpiox_bsrr
+                    .write(|w| w.bits(1 << (i + 16)))
+            };
+        }
+
+        for _ in 0..1_000_000 {
+            cortex_m::asm::nop();
+        }
     }
 }
